f48101bad9c78b67b93d2cb8245055e872e23411
MaxStolovich <widebonanza@gmail.com>
2015-12-22 14:36:47 +0200

    i've delegated run method

---
+++ Patterns Aquisition/Patterns Aquisition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns_Aquisition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Runner().run();
+
+        }
+
+
+
+        private class Runner
+        {
+            public Runner()
+            {
+            }
+
+            public void run()
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
