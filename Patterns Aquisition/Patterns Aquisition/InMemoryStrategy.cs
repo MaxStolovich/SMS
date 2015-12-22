d5dc010b6c5dae6228ed2afbe212dbbd56953fad
MaxStolovich <widebonanza@gmail.com>
2015-12-22 16:24:33 +0200

    Now there is two strategies!

---
+++ Patterns Aquisition/Patterns Aquisition/InMemoryStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns_Aquisition
+{
+    class InMemoryStrategy : Strategy
+    {
+        private List<string> messages = new List<string>();
+
+
+        public void Print(string message)
+        {
+            messages.Add(message);
+        }
+
+        
+
+           
+        }
+
+    }
+}
